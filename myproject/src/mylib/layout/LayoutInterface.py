99c22605774a00f54a0e3466a11e4c4e85cd3088
suteanfx <suteanfx@1b1820d3-2a4c-0410-a67b-c9d7efa3775d>
2008-09-17 11:07:39 +0000

    -remove old style dir tree

--- myproject/src/mylib/layout/LayoutInterface.py
+++
@@ -1,7 +0,0 @@
-class LayoutInterface(object):
-    def __init__(self):
-        raise NotImplementedError
-    def getGraph(self):
-        raise NotImplementedError
-    def getLayoutName(self):
-        raise NotImplementedError
