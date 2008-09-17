a5feed22e8a1886427aa37efb10023528f5de5c3
suteanfx <suteanfx@1b1820d3-2a4c-0410-a67b-c9d7efa3775d>
2008-09-17 11:01:40 +0000

    -remove unuse file -can not do automatic add plug-in -"- (so sad)  -can someone help me. -restore to older version to use libs

--- myproject/src/config.py
+++
@@ -1,6 +0,0 @@
-plug_ins_layout = [#"dir.filename.classname"
-                   "",
-                   ""]
-plug_ins_algorithm = ["mylib.HighestDegreeNode.HighestDegreeNode",
-                      "mylib.LeafNode.LeafNode"
-                      ]
