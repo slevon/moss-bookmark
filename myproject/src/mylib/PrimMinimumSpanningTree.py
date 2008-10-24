ebdcaec481c8ca24898edc9cc2c776eef3195593
suteanfx <suteanfx@1b1820d3-2a4c-0410-a67b-c9d7efa3775d>
2008-10-24 06:20:10 +0000

    Prim algorithm complete -create myselft not sure for bug Add Shortest path algorithm -just create file "ShortestPath"

--- myproject/src/mylib/PrimMinimumSpanningTree.py
+++ myproject/src/mylib/PrimMinimumSpanningTree.py
@@ -22,14 +22,46 @@ class PrimMinimumSpanningTree:
             if nweight < min:
                 min = nweight
                 minedge = (root,neb)
+        print "Root",root
+        print "First edge",minedge
+        self.addEdge(minedge, resultGraph)
 
+        while len(resultGraph) < len(graph):
+            minedge = self.featherEdge(resultGraph, graph, weigthedGraph)
+            print "New edge",minedge
+            if minedge != ():
+                self.addEdge(minedge, resultGraph)
+
+        '''
+        for visited in resultGraph:
+            for neb in graph[visited]:
+                print "Friend of root",neb
+                if not neb in resultGraph:
+                    nweight = weigthedGraph.getWeight(visited,neb)
+                    if nweight < min:
+                        min = nweight
+                        minedge = (visited,neb)
+        '''
+        print "New edge is",minedge
         self.addEdge(minedge, resultGraph)
         #print root,minedge
         return resultGraph
-
+    def featherEdge(self,visitedGraph,graph,weigthedGraph):
+        INFINITY = ()
+        min = nweight = INFINITY
+        minedge = ()
+        for visited in visitedGraph:
+            for neb in graph[visited]:
+                print "Friend of root",neb
+                if not neb in visitedGraph:
+                    nweight = weigthedGraph.getWeight(visited,neb)
+                    if nweight < min:
+                        min = nweight
+                        minedge = (visited,neb)
+        return minedge
     def addEdge(self,edge,graph):
         if edge[0] in graph:
-            graph[source].append(edge[1])
+            graph[edge[0]].append(edge[1])
         else:#create new node
             graph[edge[0]] =[edge[1]]
 
@@ -38,72 +70,3 @@ class PrimMinimumSpanningTree:
         else:#create new node
             graph[edge[1]] = [edge[0]]
         return graph
-    def minimal_spanning_tree(graph, root=None):
-        visited = []                    # List for marking visited and non-visited nodes
-        spanning_tree = {}              # Minimal Spanning tree
-
-        # Initialization
-        if (root is not None):
-                visited.append(root)
-                nroot = root
-                spanning_tree[root] = None
-        else:
-                nroot = 1
-
-        # Algorithm loop
-        while (nroot is not None): #if new root is None mean it no node to go
-                ledge = lightest_edge(graph, visited)
-                if (ledge == (-1, -1)):
-                        if (root is not None):
-                                break
-                        nroot = first_unvisited(graph, visited)
-                        if (nroot is not None):
-                                spanning_tree[nroot] = None
-                        visited.append(nroot)
-                else:
-                        spanning_tree[ledge[1]] = ledge[0]
-                        visited.append(ledge[1])
-
-        return spanning_tree
-
-
-    def first_unvisited(graph, visited):
-        """
-        Return first unvisited node.
-
-        @type  graph: graph
-        @param graph: Graph.
-
-        @type  visited: list
-        @param visited: List of nodes.
-
-        @rtype:  node
-        @return: First unvisited node.
-        """
-        for each in graph:
-                if (each not in visited):
-                        return each
-        return None
-    def lightest_edge(graph, visited):
-        """
-        Return the lightest edge in graph going from a visited node to an unvisited one.
-
-        @type  graph: graph
-        @param graph: Graph.
-
-        @type  visited: list
-        @param visited: List of nodes.
-
-        @rtype:  tuple
-        @return: Lightest edge in graph going from a visited node to an unvisited one.
-        """
-        lightest_edge = (-1, -1)
-        weight = -1
-        for each in visited:
-                for other in graph[each]:
-                        if (other not in visited):
-                                w = graph.get_edge_weight(each, other)
-                                if (w < weight or weight < 0):
-                                        lightest_edge = (each, other)
-                                        weight = w
-        return lightest_edge
