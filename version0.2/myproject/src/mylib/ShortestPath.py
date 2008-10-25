a1a8e8a8e50ce4f63333ca927d77f30dd4809593
suteanfx <suteanfx@1b1820d3-2a4c-0410-a67b-c9d7efa3775d>
2008-10-25 11:26:34 +0000

    Version 0.2 with graph force layout

---
+++ version0.2/myproject/src/mylib/ShortestPath.py
@@ -0,0 +1,10 @@
+'''
+Dijkstra's algorithm for shortest paths
+'''
+class ShortestPath:
+    def __init__(self):
+        pass
+    def getName(self):
+        return "Dijkstra Minimum Spanning Tree"
+    def getResult(self,weigthedGraph,source,dest):
+        pass
